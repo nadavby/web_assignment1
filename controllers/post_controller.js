69970b9e561d79e62df9016d1bf529f0b48cfc36
Nadav <nadaviku@gmail.com>
2024-11-25 21:07:54 +0200

    added getPostById function,routing and rest request

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -12,11 +12,22 @@ try{
      const Posts = await Post.find();
     return res.status(200).send(Posts);
     }
-   
 }
-
 catch(err){
     return res.status(400).send(err.message);
 }};
 
-module.exports = {getAllPosts}; 
+const getPostById = async (req, res) => {
+    const idfilter = req.params.id;
+    try{
+        const Posts = await Post.findById(idfilter);
+        if(!Posts){
+            return res.status(404).send({ error: "Post not found" });
+        }
+        return res.status(201).send(Posts);
+    } catch(err){
+        return res.status(400).send(err.message);
+    }
+   };
+
+module.exports = {getAllPosts, getPostById}; 
--- routes/Post_routes.js
+++ routes/Post_routes.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const router =express.Router();
 const Post = require("../controller/post_controller");  
+
 router.get("/", Post.getAllPosts);
+
+router.get("/:id", Post.getPostById);
+
+module.exports = router;
