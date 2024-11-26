a33e3341973bcc0c2e7f97342a9c47f1c3eee2f2
sachamak <128730251+sachamak@users.noreply.github.com>
2024-11-26 23:20:30 +0200

    fixe some bug from comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,25 +2,26 @@ const Comment = require("../models/comments_model.js");
 
 const createComment = async (req, res) => {
     try {
+       
         const { postId, content, author } = req.body;
         if (!postId || !content || !author) {
-            return res.status(400).json({ error: "postId, content, and author are required" });
+            return res.status(400).send({ error: "postId, content, and author are required" });
         }
         const newComment = await Comment.create({ postId: postId,content: content,author: author });
-        return res.status(201).json(newComment);
+        return res.status(201).send(newComment);
     } catch (err) {
-        return res.status(500).json({ error: "postId is not valid" });
+    return res.status(500).send( err.message );
     }
 };
 const getCommentById = async (req, res) => {
-    const { id } = req.params;
+    const  filterid  = req.params.id;
 
     try {
-        const comment = await Comment.findById(id);
-        if (!comment) {
+        const com = await Comment.findById(filterid);
+        if (!com) {
             return res.status(404).send({ error: "Comment not found" });
         }
-        return res.status(200).send(comment);
+        return res.status(200).send(com);
     } catch (err) {
         return res.status(500).send({ error: err.message });
     }
